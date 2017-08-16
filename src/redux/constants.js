be5edb18f653b4f7801d2a4995e1569c690c0b31
tlnob <thais.nobre7@gmail.com>
2017-08-16 16:13:12 -0300

    .gitgnore up to date

--- src/redux/constants.js
+++
@@ -1,5 +0,0 @@
-export const ADD_TASK = 'ADD_TASK';
-export const DELETE_TASK = 'DELETE_TASK';
-export const SET_TASK = 'SET_TASK';
-export const FILTER_TASK = 'FILTER_TASK';
-export const CHECKED_TASK = 'CHECKED_TASK';
--- src/redux/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from 'redux';
-import {toDoReducer} from './toDoReducer'
-
-export const reducer = combineReducers({
-	tasks: toDoReducer,  /*o state vem daqui - ou seja, o que é passado para a store*/
-});
